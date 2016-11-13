02c8246a4522655adb847f84b19f838e959445c3
JohnVonNeumann <louiswillcock@gmail.com>
2016-11-13 05:34:52 +0000

    created a contact action and view and generated more tests, changed root url from application cont and hello action to static pages controller and home action, should be final commit to this repo, this has also been pushed to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
   # REMEMBER CONTROLLER/ACTION or CONTROLLER#ACTION
   # the use of GET is an example of HTTP requests, in this case when we gen
   # a home action inside the static pages contr we autom get a page at the 
   # address /staticpages/home
-  root 'application#hello'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
